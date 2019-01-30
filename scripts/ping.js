02c5b7298676b04942b4c969207b164b0c2fd876
losiz17 <riokam.11.12@docomo.ne.jp>
2019-01-31 00:09:18 +0900

    first commit

--- scripts/ping.js
+++ scripts/ping.js
@@ -8,8 +8,42 @@
 'use strict';
 
 module.exports = (robot) => {
+  const https = require('https');
+ 
+
+  var gurunaviAPI = 'e0c8d42fc13d93b856b036308c97368c';//取得したアクセスキー
+  var url = 'https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=e0c8d42fc13d93b856b036308c97368c&latitude=36.228067&longitude=137.969049&hit_per_page=5';
+  console.log(url);
+  // url = url + 'keyid='+ gurunaviAPI;
+  // url = url + '&freeword=ラーメン';
+
   robot.respond(/PING$/i, (res) => {
     res.send('PONG');
+  });
+
+  robot.respond(/a$/i, (res) => {
+    const req = https.request(url, (res) => {
+      // console.log(res);
+      let body = '';
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
+      res.on('end', () => {
+        res = JSON.parse(body);
+        for (let i = 0; i < Object.keys(res.rest).length; i++) { 
+          console.log(res.rest[i].name);
+        }
+          
+      });
+    })
+
+    req.on('error', (e) => {
+      console.error(`problem with request: ${e.message}`);
+    });
+
+    req.end();
+    
   });
 
   robot.respond(/ADAPTER$/i, (res) => {
